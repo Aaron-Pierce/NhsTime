a4135ad9ebabb55b3c0cce07f3ce3b1c60143453
Aaron <amaxpierce@yahoo.com>
2016-03-18 13:24:06 -0500

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Aaron on 3/11/2016.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+
+});
+
+module.exports = app;
